28b0665e613680177e4f0eb6b89ba58b91908c2e
Victor Llamas Olivares <victorllamas@gmail.com>
2019-07-27 21:02:28 -0500

    Add resultCard component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import Signin from './components/signin/'
 import Header from './components/header/'
 import Homepage from './components/homepage/'
 import Filter from './components/filters/'
+import ResultCard from './components/resultCard/'
 
 class App extends Component {
     render() {
     return (
       <div>
-        <Header />
+        <ResultCard />
       </div>
     )
   }
---
+++ src/components/resultCard/index.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import image from '../../images/test.jpg'
+class ResultCard extends Component{
+
+    constructor(props){
+        super(props)
+        this.state = {
+            imgPath: image,
+            title: 'Departamento Ciudad Granja 3 Rec Nuevo en Venta',
+            location: 'zapopan, Jalisco',
+            price: '$3,170,000',
+            params: {
+                const: '116m2',
+                bedrooms:'3 recamaras',
+                restrooms: '2 baños',
+            },
+            timePublished: 'publicado hace 1 hora',
+        }
+    }
+
+    render(){
+        return(
+            <div className="card" style={{width:'18rem'}}>
+                <img className="card-img-top" src={this.state.imgPath} alt="Card image cap"/>
+                <div className="card-body">
+                    <h5 className="card-title">{this.state.title}</h5>
+                    <h6 class="card-subtitle mb-3 text-muted">{this.state.location}</h6>
+                    <h4 className="card-title">{this.state.price}</h4>
+                    <p className="card-text">{this.state.params.const} | {this.state.params.bedrooms} | {this.state.params.restrooms}</p>
+                </div>
+                <div class="card-footer text-muted"> {this.state.timePublished}</div>
+            </div> 
+        )
+    }
+}
+
+export default ResultCard
