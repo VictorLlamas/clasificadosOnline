5d2853b0e615ac881dd1db8885d8a27d62aa0e52
Victor Llamas Olivares <victorllamas@gmail.com>
2019-07-29 00:16:09 -0500

    Delete image

--- src/components/notFound/index.js
+++ src/components/notFound/index.js
@@ -1,15 +1,9 @@
 import React from 'react'
-import { Image } from 'react-bootstrap'
-
-import img404 from '../../images/404-page.png'
 
 function NotFound(){
     return(
         <div>
-            <Image
-                src= {img404}
-                alt= "Page not Found"
-                className="img-responsive center-block" />
+            Not Found
         </div>
     )
 }
