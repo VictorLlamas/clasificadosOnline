8615d7528674fafcfa39a57fbd5ab6bcb58b2944
Victor Llamas Olivares <victorllamas@gmail.com>
2019-08-12 21:39:09 -0500

    Updates

---
+++ precache-manifest.ca7c7f53812adaa8741fc7e9d6ffc4ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34304464b00ecc78b97e129eed145283",
+    "url": "/clasificadosOnline/index.html"
+  },
+  {
+    "revision": "decccccedc30007015a2",
+    "url": "/clasificadosOnline/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "01ab5abe86eae22dab63",
+    "url": "/clasificadosOnline/static/css/main.33fe8dab.chunk.css"
+  },
+  {
+    "revision": "decccccedc30007015a2",
+    "url": "/clasificadosOnline/static/js/2.b4892f28.chunk.js"
+  },
+  {
+    "revision": "01ab5abe86eae22dab63",
+    "url": "/clasificadosOnline/static/js/main.6a2212c0.chunk.js"
+  },
+  {
+    "revision": "363f77ad97c2e13c0035",
+    "url": "/clasificadosOnline/static/js/runtime~main.566b5c0e.js"
+  },
+  {
+    "revision": "5748d930c435886505f780f2d98dd379",
+    "url": "/clasificadosOnline/static/media/test.5748d930.jpg"
+  }
+]);
